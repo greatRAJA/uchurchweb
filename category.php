7734daa3e210bd794deb184401d3afa3023bb419
greatRAJA <snadarajan@hotmail.com>
2014-05-11 18:25:06 -0400

    added new category template when user clicks on category

---
+++ category.php
@@ -0,0 +1,36 @@
+<?php
+/**
+* A Simple Category Template
+*/
+get_header(); ?> 
+
+<div class="container">
+	<div class="col-md-8">
+	<?php 
+	// Check if there are any posts to display
+		if ( have_posts() ) : ?>
+			<header class="archive-header">
+			<h1 class="archive-title"><?php single_cat_title( '', true ); ?> series</h1>
+			</header>
+
+			<?php
+			// The Loop
+			while ( have_posts() ) : the_post(); ?>
+			<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+			<small><?php the_time('F jS, Y') ?> by <?php the_author_posts_link() ?></small>
+
+			<div class="entry">
+			<?php the_excerpt(); ?>
+			</div>
+			<?php endwhile; // End Loop
+			else: ?>
+			<p>Sorry, no posts matched your criteria.</p>
+		<?php endif; ?>
+	</div>
+	<div class="col-md-4">
+		<?php get_sidebar(); ?>
+	</div>
+</div>
+
+<?php get_footer(); ?>
+
