7d3b911cbdaea9da781d7a3a88e34fe3e5f44b45
greatRAJA <snadarajan@hotmail.com>
2014-05-11 18:25:30 -0400

    each post now displays inside container, not auto width

--- single.php
+++ single.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-	<div class="row">
+	<div class="container">
 		<div class="col-md-8">
 			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 				<h2><?php the_title(); ?></h2>
